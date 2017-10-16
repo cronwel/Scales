a25aba86d8c37beffb0dc65fbcaac3e387883c07
Cronwel Noel Irias <noelirias@gmail.com>
2017-10-16 15:44:06 -0500

    board_topics views +

--- boards/tests.py
+++ boards/tests.py
@@ -1,7 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
+from .views import home, board_topics
 from .views import home
+from .models import Board
 
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
@@ -12,3 +14,21 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+class BoardTopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board')
+
+    def test_board_topics_sc_200(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_sc_404(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolves_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(vview.func, board_topics)
--- boards/views.py
+++ boards/views.py
@@ -4,12 +4,8 @@ from .models import Board
 
 def home(request):
     boards = Board.objects.all()
-    # boards_names = list()
-    #
-    # for board in boards:
-    #     boards_names.append(board.name)
-    #
-    # response_html = '<br>'.join(boards_names)
-
-    # return HttpResponse(response_html)
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request,'topics.html', {'board': board})

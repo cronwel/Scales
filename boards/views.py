c45ad4ed446e2c029169c881ccb9c61cdea1421a
Cronwel Noel Irias <noelirias@gmail.com>
2017-10-18 18:34:41 -0500

    more tests for form

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 def home(request):
     boards = Board.objects.all()
@@ -14,25 +14,20 @@ def board_topics(request, pk):
 
 def new_topic(request,pk):
     board = get_object_or_404(Board, pk=pk)
-
+    user =User.objects.first()
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first()
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('board_topics', pk=board.pk)
-
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)
+    else
+        form = NewTopicForm()
     return render(request, 'new_topic.html', {'board' : board })
